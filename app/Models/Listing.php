e3da83f2061c904e319b382176777f26dbfd0fda
Md. Al-amin <sumonrahman3600@gmail.com>
2022-08-15 11:35:44 +0600

    Using Factory to data seed

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,35 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing{
-    public static function all(){
-        return [
-            [
-                'id' => 1,
-                'title' => 'One',
-                'description' => 'lorem ipsum test test test test'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Two',
-                'description' => 'lorem ipsum test test test test'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Three',
-                'description' => 'lorem ipsum test test test test'
-            ],
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach($listings as $key => $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ $listing['title'] }}</title>
 </head>
 <body>
 
-    @unless (count($listing) == 0)        
+    @unless (empty($listing))        
        
         <h1>{{ $listing['title'] }}</h1>
+        <p>{{ $listing['company'] }}</p>
+        <p>{{ $listing['email'] }}</p>
+        <p>{{ $listing['website'] }}</p>
         <p>{{ $listing['description'] }}</p>
     @else
         <p>Nothing</p>
